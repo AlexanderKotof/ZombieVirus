ea5968a6e671f36532903d47b233ae04ec14e93f
Alexandr Kotov <newbeedevinc@gmail.com>
2023-01-27 18:11:46 +0400

    InputFeature implemented

---
+++ Assets/Features/InputFeature/GameInputFeature.cs
@@ -0,0 +1,24 @@
+﻿using FeatureSystem.Features;
+using FeatureSystem.Systems;
+
+public class GameInputFeature : Feature
+{
+    public override void Initialize()
+    {
+        GameSystems.RegisterSystem(new PlayerInputSystem());
+    }
+}
+
+public class PlayerInputSystem : ISystem
+{
+    public InputController PlayerInput { get; private set; }
+
+    public void Initialize()
+    {
+        PlayerInput = new InputController();
+    }
+    public void Destroy()
+    {
+
+    }
+}
