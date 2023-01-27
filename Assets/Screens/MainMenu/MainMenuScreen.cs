76c6bab917cba7926c5018f35e64fd79fccabd0c
Alexandr Kotov <newbeedevinc@gmail.com>
2023-01-27 15:04:29 +0400

    MainMenu screen

---
+++ Assets/Screens/MainMenu/MainMenuScreen.cs
@@ -0,0 +1,9 @@
+﻿using ScreenSystem.Components;
+using ScreenSystem.Screens;
+
+public class MainMenuScreen : BaseScreen
+{
+    public ButtonComponent startButton;
+
+    public ButtonComponent exitButton;
+}
