57fdfe0ea834a1819f5167212daa6d05b75a1e81
Alexandr Kotov <newbeedevinc@gmail.com>
2023-01-27 14:54:34 +0400

    Scene loader

---
+++ Assets/Scripts/SceneLoader/SceneLoader.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using UI.Screens;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader
+{
+    private const string _gameSceneName = "Game";
+    private const string _homeSceneName = "Home";
+
+    private static Action _callback;
+
+    public static void LoadGameScene(Action callback)
+    {
+        LoadScene(_gameSceneName, callback);
+    }
+
+    public static void LoadHomeScene(Action callback)
+    {
+        LoadScene(_homeSceneName, callback);
+    }
+
+    public static void LoadScene(string sceneName, Action callback)
+    {
+        _callback = callback;
+        ScreenSystem.ScreensManager.ShowScreen<LoadingScreen>();
+
+        var operation = SceneManager.LoadSceneAsync(sceneName);
+        operation.completed += OnLoaded;
+    }
+
+    private static void OnLoaded(AsyncOperation operation)
+    {
+        operation.completed -= OnLoaded;
+
+        ScreenSystem.ScreensManager.HideScreen<LoadingScreen>();
+
+        _callback?.Invoke();
+    }
+}
