952e8c6620f3fe28e04b63fc5413804d358193d3
Alexandr Kotov <newbeedevinc@gmail.com>
2023-01-27 14:58:59 +0400

    Save and load PlayerData

---
+++ Assets/Scripts/PlayerDataManager.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using UnityEngine;
+
+public class PlayerDataManager : MonoBehaviour
+{
+    public PlayerData defaultData;
+    public static PlayerData Data { get; private set; }
+
+    private static PlayerDataManager _instance;
+
+    private void Awake()
+    {
+        DontDestroyOnLoad(this);
+        _instance = this;
+    }
+
+    public static PlayerData LoadOrCreateNewData()
+    {
+        Data = SaveGameSystem.SaveLoadData.Load();
+
+        if (Data != null)
+            return Data;
+
+        Data = _instance.defaultData;
+
+        return Data;
+    }
+
+    public static void SaveData()
+    {
+        SaveGameSystem.SaveLoadData.SaveFile(Data);
+    }
+}
+
+[Serializable]
+public class PlayerData
+{
+    public CharacterData[] characterDatas;
+    public InventoryData inventoryData;
+    public QuestsData questsData;
+}
+
+[Serializable]
+public class QuestsData
+{
+    public int[] currentQuestIds;
+}
+
+[Serializable]
+public class CharacterData
+{
+    public CharacterPrototype prototype;
+
+    public int currentExp;
+
+    public Weapon weapon;
+
+    public Armor armor;
+}
+
+[Serializable]
+public class InventoryData
+{
+    public int[] itemsIds;
+}
---
+++ Assets/Scripts/SaveLoadData.cs
@@ -0,0 +1,58 @@
+﻿using System.IO;
+using UnityEngine;
+
+namespace SaveGameSystem
+{
+    public static class SaveLoadData
+    {
+        static string SaveFilename = "savedGame1.json";
+        public static string pathFormat = null;
+
+        /// <summary>
+        /// Write save file on disc
+        /// </summary>
+        /// <param name="saveData"></param>
+        public static void SaveFile(PlayerData saveData)
+        {
+            if (pathFormat == null)
+            {
+#if UNITY_ANDROID && !UNITY_EDITOR
+                pathFormat =   Application.persistentDataPath + "/" + SaveFilename;
+#else
+                pathFormat = Application.dataPath + "/" + SaveFilename;
+#endif
+            }
+
+            string dataToJson = JsonUtility.ToJson(saveData);
+            File.WriteAllText(pathFormat, dataToJson);
+        }
+
+        public static PlayerData Load()
+        {
+            if (pathFormat == null)
+            {
+#if UNITY_ANDROID && !UNITY_EDITOR
+                pathFormat =   Application.persistentDataPath + "/" + SaveFilename;
+#else
+                pathFormat = Application.dataPath + "/" + SaveFilename;
+#endif
+            }
+
+            if (File.Exists(pathFormat))
+            {
+                PlayerData data = JsonUtility.FromJson<PlayerData>(File.ReadAllText(pathFormat));
+                if (data != null)
+                    Debug.Log("Saved data loaded");
+                return data;
+            }
+            else
+            {
+                Debug.Log("Cannot load saved data");
+                return null;
+            }
+
+        }
+
+    }
+}
+
