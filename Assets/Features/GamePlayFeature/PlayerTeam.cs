3e4ec9641c8f2d875ebb735a1ffe3d72cab73729
Alexandr Kotov <newbeedevinc@gmail.com>
2023-01-27 18:22:52 +0400

    CharactersSystem moved to CharacterFeature folder

---
+++ Assets/Features/GamePlayFeature/PlayerTeam.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using UnityEngine;
+
+[Serializable]
+public class PlayerTeam
+{
+    public PlayerComponent[] characters { get; private set; }
+
+    public CharacterData[] charactersData;
+
+    public void InstatiateCharacters(Transform spawnPoint)
+    {
+        charactersData = PlayerDataManager.Data.characterDatas;
+
+        characters = new PlayerComponent[charactersData.Length];
+
+        for (int i = 0; i < charactersData.Length; i++)
+        {
+            var position = spawnPoint.position + Vector3.right * i;
+            characters[i] = CharacterSpawnUtils.SpawnCharacter(charactersData[i], position, spawnPoint.rotation) as PlayerComponent;
+        }
+    }
+
+
+}
---
+++ Assets/Features/GamePlayFeature/PlayerTeamSystem.cs
@@ -0,0 +1,35 @@
+﻿using FeatureSystem.Systems;
+
+public class PlayerTeamSystem : ISystem
+{
+    public PlayerTeam team;
+
+    public int selectedCharacters = -1;
+
+    public void Initialize()
+    {
+        var sceneContext = SceneContext.GetSceneContext();
+        team = new PlayerTeam();
+        team.InstatiateCharacters(sceneContext.playerSpawnPoint);
+    }
+
+    public CharacterComponent[] GetSelectedCharacters()
+    {
+        if (selectedCharacters == -1)
+        {
+            return team.characters;
+        }
+        else
+        {
+            return new[] { team.characters[selectedCharacters] };
+        }
+    }
+
+
+    public void Destroy()
+    {
+        // destroy characters
+    }
+
+
+}
