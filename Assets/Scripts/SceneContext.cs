a63188f7dc1c1acb52426dd23abac74de8994708
Alexandr Kotov <newbeedevinc@gmail.com>
2023-01-27 14:59:20 +0400

    SceneContext impemented

---
+++ Assets/Scripts/SceneContext.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SceneContext : MonoBehaviour
+{
+    public SpawnPointComponent[] enemiesSpawnPoints;
+
+    public Transform playerSpawnPoint;
+
+    public Transform exitPoint;
+
+}
