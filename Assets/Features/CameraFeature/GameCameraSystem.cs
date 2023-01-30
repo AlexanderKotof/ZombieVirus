f4e81d09051b7829264742f56dffd5b9edef1ba9
Alexandr Kotov <newbeedevinc@gmail.com>
2023-01-30 16:49:39 +0400

    Camera Feature implemented

---
+++ Assets/Features/CameraFeature/GameCameraSystem.cs
@@ -0,0 +1,23 @@
+﻿using FeatureSystem.Systems;
+
+public class GameCameraSystem : ISystem
+{
+    public CameraRigComponent Camera { get; private set; }
+    private CameraRigComponent _cameraPrefab;
+
+    public GameCameraSystem(CameraRigComponent cameraPrefab)
+    {
+        _cameraPrefab = cameraPrefab;
+    }
+
+    public void Destroy()
+    {
+        CameraRigComponent.Destroy(Camera);
+    }
+
+    public void Initialize()
+    {
+        Camera = CameraRigComponent.Instantiate(_cameraPrefab, null);
+    }
+}
+
