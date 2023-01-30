7c823cde64f0403fb86cec48a4bb8bd7c0b32cc2
Alexandr Kotov <newbeedevinc@gmail.com>
2023-01-30 16:46:57 +0400

    SceneContext camera bounds added

--- Assets/Scripts/SceneContext/SceneContext.cs
+++ Assets/Scripts/SceneContext/SceneContext.cs
@@ -8,6 +8,8 @@ public class SceneContext : MonoBehaviour
 
     public Transform exitPoint;
 
+    public Bounds cameraBounds;
+
     private static SceneContext _instance { get; set; }
 
     public void Awake()
@@ -25,4 +27,22 @@ public class SceneContext : MonoBehaviour
         _instance = null;
     }
 
+    private void OnDrawGizmosSelected()
+    {
+        Vector3[] points = new Vector3[]
+        {
+            cameraBounds.max,
+            cameraBounds.max - Vector3.forward * cameraBounds.extents.z * 2,
+            cameraBounds.min,
+            cameraBounds.min + Vector3.forward * cameraBounds.extents.z * 2,
+        };
+
+        for (int i = 0; i < points.Length; i++)
+        {
+            if (i < points.Length - 1)
+                Debug.DrawLine(points[i], points[i + 1], Color.green);
+            else
+                Debug.DrawLine(points[i], points[0], Color.green);
+        }
+    }
 }
