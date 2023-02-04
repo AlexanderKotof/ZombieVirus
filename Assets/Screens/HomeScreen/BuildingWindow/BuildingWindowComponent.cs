5efc8dbc246df4e5bc26e4de3ab497a92dd78ba6
Alexandr Kotov <newbeedevinc@gmail.com>
2023-02-04 10:16:05 +0400

    Buildings Window WIP

---
+++ Assets/Screens/HomeScreen/BuildingWindow/BuildingWindowComponent.cs
@@ -0,0 +1,11 @@
+using ScreenSystem.Components;
+using System.Collections;
+
+namespace Screens.HomeScreen.BuildingWindow
+{
+    public class BuildingWindowComponent : WindowComponent
+    {
+        public ListComponent buildingsList;
+
+    }
+}
